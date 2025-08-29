7a445799039c42af90c30422512cc231d89b9f2d
kekley <111942843+kekley@users.noreply.github.com>
2025-08-29 15:27:31 -0700

    add world module

---
+++ src/world/mod.rs
@@ -0,0 +1,21 @@
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+
+use std::error::Error;
+
+pub struct World {
+    folder: PathBuf,
+    regions: Vec<PathBuf>,
+}
+
+impl World {
+    pub fn new(path: &str) -> Result<Self, Box<dyn Error>> {
+        let path = PathBuf::from_str(path)?;
+
+        let read_dir = std::fs::read_dir(path)?;
+
+        todo!()
+    }
+}
